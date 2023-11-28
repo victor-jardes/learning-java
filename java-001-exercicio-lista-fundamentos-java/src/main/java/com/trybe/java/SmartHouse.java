08f91e1074d91e6222bb4e5dcb5812f5c601277f
victor <victor_maanoel@outlook.com>
2023-11-28 18:55:49 -0400

    feature(finished): finished 4 challenges

--- java-001-exercicio-lista-fundamentos-java/src/main/java/com/trybe/java/SmartHouse.java
+++ java-001-exercicio-lista-fundamentos-java/src/main/java/com/trybe/java/SmartHouse.java
@@ -1,8 +1,33 @@
 package com.trybe.java;
 
-/**
- * Classe.
- **/
 public class SmartHouse {
+  public boolean lamp;
+
+  public SmartHouse() {
+    this.lamp = false;
+  }
+
+  public void ligarLampada() {
+    this.lamp = true;
+  }
+
+  public void desligarLampada() {
+    this.lamp = false;
+  }
+
+  public boolean isLampadaLigada() {
+    return this.lamp;
+  }
+
+  /**
+   * `conectarInternet` method, this method return true or false.
+   */
+  public boolean conectarInternet(double connectionRate) {
+    if (connectionRate > 0.5) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
